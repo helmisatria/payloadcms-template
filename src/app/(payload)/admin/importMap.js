f28a348627f556afc9dfe94c975bf9eaa4cacf92
Helmi Satria <satriahelmi@gmail.com>
2026-04-05 12:30:05 +0700

    fix: types

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -4,10 +4,8 @@ import { Logo as Logo_919492c8e60179286a0f183c0a5b652e } from '../../../componen
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
 export const importMap = {
-  '/components/BetterAuthAvatar#BetterAuthAvatar':
-    BetterAuthAvatar_69b977cd49fd9c85135135d6378fac75,
-  '/components/BetterAuthLogout#BetterAuthLogoutButton':
-    BetterAuthLogoutButton_24b45fb9e8cbd92d6492dcb6694c2b8d,
-  '/components/Logo#Logo': Logo_919492c8e60179286a0f183c0a5b652e,
-  '@payloadcms/next/rsc#CollectionCards': CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1,
+  "/components/BetterAuthAvatar#BetterAuthAvatar": BetterAuthAvatar_69b977cd49fd9c85135135d6378fac75,
+  "/components/BetterAuthLogout#BetterAuthLogoutButton": BetterAuthLogoutButton_24b45fb9e8cbd92d6492dcb6694c2b8d,
+  "/components/Logo#Logo": Logo_919492c8e60179286a0f183c0a5b652e,
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
